5708afa5d42d6edef77fcb36e72e0fdefa40c4fd
xxD04xx <dominic.ritzmann@noseryoung.ch>
2022-06-14 15:34:02 +0200

    routing done

--- sneakershop/src/App.tsx
+++ sneakershop/src/App.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import DetailedProductPage from "./Pages/DetailedProductPage";
 import HomePage from "./Pages/HomePage";
+import Login from "./Pages/Login";
+import ProductPage from "./Pages/ProductPage";
+import Register from "./Pages/Register";
+import ShoppingCart from "./Pages/ShoppingCart";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
+        <Route path="/Products" element={<ProductPage />}></Route>
+        <Route path="/Product" element={<DetailedProductPage />}></Route>
+        <Route path="/ShoppingCart" element={<ShoppingCart />}></Route>
+        <Route path="/Login" element={<Login />}></Route>
+        <Route path="/Register" element={<Register />}></Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ sneakershop/src/Pages/DetailedProductPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DetailedProductPage = () => {
+  return <div>DetailedProductPage</div>;
+};
+
+export default DetailedProductPage;
