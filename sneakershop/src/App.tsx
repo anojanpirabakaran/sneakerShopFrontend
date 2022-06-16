c77a0ee5bf6e55307c56f80e3603fd93c9d12619
xxD04xx <dominic.ritzmann@noseryoung.ch>
2022-06-16 11:28:09 +0200

    added new typescript Project

--- sneakershop/src/App.tsx
+++ sneakershop/src/App.tsx
@@ -1,120 +1,26 @@
-<<<<<<< HEAD
-import { Badge, Drawer, Grid, LinearProgress } from "@material-ui/core";
-import { AddShoppingCart } from "@material-ui/icons";
-import { useState } from "react";
-import { useQuery } from "react-query";
-import { Wrapper, StyledButton } from "./App.styles";
-import Item from "./organisms/Item/Item";
-import Cart from "./organisms/Cart/Cart";
-import "./styles.css";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-export type CartItemType = {
-  id: number;
-  category: string;
-  description: string;
-  image: string;
-  price: number;
-  title: string;
-  amount: number;
-};
-
-const getProducts = async (): Promise<CartItemType[]> =>
-  await (await fetch("https://fakestoreapi.com/products")).json();
-
-export default function App() {
-  const [cartOpen, setCartOpen] = useState(false);
-  const [cartItems, setCartItems] = useState<CartItemType[]>([]);
-
-  const { data, isLoading, error } = useQuery<CartItemType[]>(
-    "products",
-    getProducts
-  );
-
-  const getTotalItems = (items: CartItemType[]) =>
-    items.reduce((acc, item) => acc + item.amount, 0);
-
-  const handleAddToCart = (clickedItem: CartItemType) => {
-    setCartItems((prev) => {
-      const isItemInCart = prev.find((item) => item.id === clickedItem.id);
-
-      if (isItemInCart) {
-        return prev.map((item) =>
-          item.id === clickedItem.id
-            ? { ...item, amount: item.amount + 1 }
-            : item
-        );
-      }
-
-      return [...prev, { ...clickedItem, amount: 1 }];
-    });
-  };
-
-  const handleRemoveFromCart = (id: number) => {
-    setCartItems((prev) =>
-      prev.reduce((acc, item) => {
-        if (item.id === id) {
-          if (item.amount === 1) return acc;
-          return [...acc, { ...item, amount: item.amount - 1 }];
-        } else {
-          return [...acc, item];
-        }
-      }, [] as CartItemType[])
-    );
-  };
-
-  if (isLoading) return <LinearProgress />;
-  if (error) return <div>Something went wrong</div>;
-
+function App() {
   return (
-    <Wrapper>
-      <Drawer anchor="right" open={cartOpen} onClose={() => setCartOpen(false)}>
-        <Cart
-          cartItems={cartItems}
-          addToCart={handleAddToCart}
-          removeFromCart={handleRemoveFromCart}
-        />
-      </Drawer>
-
-      <StyledButton onClick={() => setCartOpen(true)}>
-        <Badge badgeContent={getTotalItems(cartItems)} color="error">
-          <AddShoppingCart />
-        </Badge>
-      </StyledButton>
-
-      <Grid container spacing={3}>
-        {data?.map((item) => (
-          <Grid item key={item.id} xs={12} sm={4}>
-            <Item item={item} handleAddToCart={handleAddToCart} />
-          </Grid>
-        ))}
-      </Grid>
-    </Wrapper>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
-=======
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import DetailedProductPage from "./Pages/DetailedProductPage";
-import HomePage from "./Pages/HomePage";
-import Login from "./Pages/Login";
-import ProductPage from "./Pages/ProductPage";
-import Register from "./Pages/Register";
-import ShoppingCart from "./Pages/ShoppingCart";
-
-const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />}></Route>
-        <Route path="/Products" element={<ProductPage />}></Route>
-        <Route path="/Product" element={<DetailedProductPage />}></Route>
-        <Route path="/ShoppingCart" element={<ShoppingCart />}></Route>
-        <Route path="/Login" element={<Login />}></Route>
-        <Route path="/Register" element={<Register />}></Route>
-      </Routes>
-    </BrowserRouter>
-  );
-};
 
 export default App;
->>>>>>> 3761a6a42870cc5a8acb2f99b571d152b1e22b62
--- sneakershop/src/Pages/DetailedProductPage.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const DetailedProductPage = () => {
-  return <div>DetailedProductPage</div>;
-};
-
-export default DetailedProductPage;
