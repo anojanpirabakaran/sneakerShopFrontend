d4d7fc684e3df7aebcdcc58efa8dabf9cd47a3f5
xxD04xx <dominic.ritzmann@noseryoung.ch>
2022-06-21 11:04:00 +0200

    added navbar

--- sneakershop/src/App.tsx
+++ sneakershop/src/App.tsx
@@ -1,16 +1,23 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
+import { Container, Navbar, Nav } from "react-bootstrap";
+import MuiButton from "./atoms/MuiButton/MuiButton";
+import MuiNavbar from "./atoms/MuiNavbar/MuiNavbar";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <MuiNavbar />
+
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ sneakershop/src/atoms/MuiButton/MuiButton.tsx
@@ -0,0 +1,16 @@
+import { Button, ButtonProps } from '@mui/material'
+import React from 'react'
+
+/**
+ * Wrapper for the mui Button component. Sets the default variant to 'contained'
+ * 
+ * @param props The props of the button, same as mui button
+ * @returns the TSX component
+ */
+const MuiButton = (props: ButtonProps) => {
+  return (
+    <Button variant={props.variant || "contained"} {...props} />
+  )
+}
+
+export default MuiButton;
