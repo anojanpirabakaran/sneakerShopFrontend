5f02f7e104c0f178336eb86ec6bd683460f8a21a
xxD04xx <dominic.ritzmann@noseryoung.ch>
2022-06-22 08:47:10 +0200

    removed buy now button

--- sneakershop/src/pages/Homepage/Homepage.tsx
+++ sneakershop/src/pages/Homepage/Homepage.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import MuiCarousel from "../../atoms/Carousel/Carousel";
-import MuiButton from "../../atoms/MuiButton/MuiButton";
 import "./Homepage.css";
 
 export default function Homepage() {
   return (
     <>
       <MuiCarousel />
-      <MuiButton className="buyButton">Buy now</MuiButton>
     </>
   );
 }
