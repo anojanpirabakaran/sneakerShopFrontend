df832a7fd697accc7607f492fd4b5f4d0cd91e25
ProgrammStude <shawn.lacarta@noseryoung.ch>
2022-06-23 14:02:19 +0200

    changed background color and added container on cards

--- sneakershop/src/pages/Shop/Shop.tsx
+++ sneakershop/src/pages/Shop/Shop.tsx
@@ -1,8 +1,13 @@
+import { Paper } from '@mui/material'
 import React from 'react'
 import ItemCard from '../../organisms/ItemCard/ItemCard'
 
+import "./Shop.css"
+
 export default function Shop() {
   return (
+    <Paper elevation={2} className="paper_background">
     <ItemCard/>
+    </Paper>
   )
 }
