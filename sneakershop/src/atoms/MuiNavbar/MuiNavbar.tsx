bf3b7cca5ff06c22f44a5bfd4e01ea02cee8d881
xxD04xx <dominic.ritzmann@noseryoung.ch>
2022-06-23 09:38:22 +0200

    removed unused import

--- sneakershop/src/atoms/MuiNavbar/MuiNavbar.tsx
+++ sneakershop/src/atoms/MuiNavbar/MuiNavbar.tsx
@@ -11,7 +11,6 @@ import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
-import AdbIcon from "@mui/icons-material/Adb";
 import "./MuiNavbar.css";
 import logo from "../../images/Logo.png";
 import HomeIcon from "@mui/icons-material/Home";
