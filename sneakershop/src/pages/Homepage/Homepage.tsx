001a9e7c88bb4fddf846a9cd3a50be07bbafa619
xxD04xx <dominic.ritzmann@noseryoung.ch>
2022-06-29 13:56:29 +0200

    fixed about page

--- sneakershop/src/pages/Homepage/Homepage.tsx
+++ sneakershop/src/pages/Homepage/Homepage.tsx
@@ -1,20 +1,18 @@
 import React from "react";
-import MuiCarousel from "../../atoms/Carousel/Carousel";
 import "./Homepage.css";
-import image from "../../images/Jordan11.png"
+import image from "../../images/Jordan11.png";
 import ItemCard from "../../organisms/ItemCard/ItemCard";
 import { Paper } from "@mui/material";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Homepage() {
   return (
     <>
-    <img className="homeImg" src={image} alt="jordan 11" />
-    <Paper elevation={0} className="paper_background">
+      <img className="homeImg" src={image} alt="jordan 11" />
+      <Paper elevation={0} className="paper_background">
         <h1 className="shop_title">Popular</h1>
         <ItemCard />
-</Paper>
-    
-    
+      </Paper>
     </>
   );
 }
