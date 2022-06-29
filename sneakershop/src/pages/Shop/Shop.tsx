63a40fec7f9068f2b0ec8b486c94d6374faa0d20
ProgrammStude <shawn.lacarta@noseryoung.ch>
2022-06-29 10:46:30 +0200

    fixed bug responsiveness with shop

--- sneakershop/src/pages/Shop/Shop.tsx
+++ sneakershop/src/pages/Shop/Shop.tsx
@@ -1,4 +1,4 @@
-import { Paper } from "@mui/material";
+import { Grid, Paper } from "@mui/material";
 import React from "react";
 import ItemCard from "../../organisms/ItemCard/ItemCard";
 
@@ -9,9 +9,11 @@ export default function Shop() {
   return (
     <>
       <img className="shopImg" src={shopImg} alt="jordan 1" />
+      <Grid container item sm={12}>
       <Paper elevation={0} className="paper_background">
         <ItemCard />
       </Paper>
+      </Grid>
     </>
   );
 }
