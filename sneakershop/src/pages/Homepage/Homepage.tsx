94eb156b2baaa41af778487216c973d1da97ca17
ProgrammStude <shawn.lacarta@noseryoung.ch>
2022-06-29 13:45:40 +0200

    edit homepage

--- sneakershop/src/pages/Homepage/Homepage.tsx
+++ sneakershop/src/pages/Homepage/Homepage.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import MuiCarousel from "../../atoms/Carousel/Carousel";
 import "./Homepage.css";
+import image from "../../images/Jordan11.png"
+import ItemCard from "../../organisms/ItemCard/ItemCard";
+import { Paper } from "@mui/material";
 
 export default function Homepage() {
   return (
     <>
-      <MuiCarousel />
+    <img className="homeImg" src={image} alt="jordan 11" />
+    <Paper elevation={0} className="paper_background">
+        <h1 className="shop_title">Popular</h1>
+        <ItemCard />
+</Paper>
+    
+    
     </>
   );
 }
